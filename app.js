233adf97f0a11dbc05cdba40d9fd59c61ccf7cc3
Sebastian <Sebastiandd87@gmail.com>
2024-09-07 16:57:34 -0500

    Add post

--- app.js
+++ app.js
@@ -4,6 +4,63 @@ const port = 3000;
 
 app.use(express.json());
 
+function readDataFromFile() {
+    if (fs.existsSync(filePath)) {
+      try {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const data = fileContent.trim() ? JSON.parse(fileContent) : [];
+        console.log('Datos leídos del archivo:', data);
+        return data;
+      } catch (error) {
+        console.error('Error al leer el archivo JSON:', error.message);
+        return []; 
+      }
+    } else {
+      console.warn('El archivo JSON no existe.');
+      return [];
+    }
+  }
+  
+  function writeDataToFile(data) {
+    try {
+      fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+      console.log('Datos escritos en el archivo:', data);
+    } catch (error) {
+      console.error('Error al escribir en el archivo JSON:', error.message);
+    }
+  }
+
+  app.post('/save', (req, res) => {
+    console.log('Solicitud POST recibida para guardar datos:', req.body);
+    const dataArray = Array.isArray(req.body) ? req.body : [req.body];
+    for (const item of dataArray) {
+      if (typeof item.id === 'undefined' || typeof item.arrivalTime === 'undefined') {
+        console.warn('Solicitud incorrecta. Cada objeto debe tener un ID y una fecha programada.');
+        return res.status(400).send('Cada objeto debe tener un ID y una fecha programada.');
+      }
+    }
+  
+    let existingData = readDataFromFile();
+    
+    for (const data of dataArray) {
+      const existingItem = existingData.find(item => item.id === data.id);
+      if (existingItem) {
+        console.warn(`El ID ${data.id} ya existe.`);
+        return res.status(400).send(`La CC ${data.id} ya existe. Intenta con otro ID.`);
+      }
+    }
+  
+    for (const data of dataArray) {
+      const arrivalTime = new Date().toISOString();
+      const newData = { ...data, arrivalTime: arrivalTime, modificationCount: 0 }; 
+      existingData.push(newData);
+    }
+  
+    writeDataToFile(existingData);
+    console.log('Datos guardados correctamente.');
+    res.send('Datos guardados correctamente.');
+  });
+
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
