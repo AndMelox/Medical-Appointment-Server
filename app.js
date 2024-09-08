c1f579d0951a46c3cf7d95a05fd51b9b6bbb44fd
AndresFelipeMelo <115041024+AndMelox@users.noreply.github.com>
2024-09-07 19:24:24 -0500

    Post Integrado

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express();
 const filePath = "citas.json";
 const port = 3000;
 
-let appointmentCounter = 0;
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
@@ -47,94 +45,108 @@ function writeDataToFile(data) {
   }
 }
 
-app.post('/save', upload.single('document'), (req, res) => {
-    console.log('Solicitud POST recibida para guardar datos:', req.body);
-  
-    const dataArray = Array.isArray(req.body) ? req.body : [req.body];
-  
-    for (const item of dataArray) {
-      if (typeof item.id === 'undefined' || typeof item.arrivalTime === 'undefined') {
-        console.warn('Solicitud incorrecta. Cada objeto debe tener un ID y una fecha programada.');
-        return res.status(400).send('Cada objeto debe tener un ID y una fecha programada.');
-      }
-    }
-  
-    let existingData = readDataFromFile();
-  
-    for (const data of dataArray) {
-      const existingItem = existingData.find(item => item.id === data.id);
-      if (existingItem) {
-        console.warn(`El ID ${data.id} ya existe.`);
-        return res.status(400).send(`La CC ${data.id} ya existe. Intenta con otro ID.`);
-      }
-  
-      const sameTimeItem = existingData.find(item => item.arrivalTime === data.arrivalTime);
-      if (sameTimeItem) {
-        console.warn(`Ya existe una cita a la misma hora y el mismo día: ${data.arrivalTime}.`);
-        return res.status(400).send(`Ya existe una cita a la misma hora y el mismo día: ${data.arrivalTime}.`);
-      }
-    }
-  
-    for (const data of dataArray) {
-      const arrivalTime = new Date().toISOString().split('T')[0] + 'T00:00:00.000Z';
-      const newData = { 
-        ...data, 
-        arrivalTime: arrivalTime,
-        image: req.file ? req.file.filename : null // Guardar el nombre del archivo de imagen
-      };
-      existingData.push(newData);
-    }
-  
-    writeDataToFile(existingData);
-    console.log('Datos guardados correctamente.');
-    res.send('Datos guardados correctamente.');
-  });
-  
+function generateUniqueId(cc) {
+  const timestamp = Date.now();
+  return `${cc}-${timestamp}`;
+}
 
-// Endpoint para obtener citas en un rango de fechas
+app.post('/save', upload.single('image'), (req, res) => {
+  console.log('Solicitud POST recibida para guardar datos:', req.body);
+
+  const dataArray = Array.isArray(req.body) ? req.body : [req.body];
+
+  for (const item of dataArray) {
+    if (typeof item.arrivalTime === 'undefined') {
+      console.warn('Solicitud incorrecta. Cada objeto debe tener una fecha programada.');
+      return res.status(400).send('Cada objeto debe tener una fecha programada.');
+    }
+
+    if (!/^\d{10}$/.test(item.cedula)) {
+      console.warn('La cédula debe tener 10 dígitos.');
+      return res.status(400).send('La cédula debe tener 10 dígitos.');
+    }
+  }
+
+  let existingData = readDataFromFile();
+
+  for (const data of dataArray) {
+    const sameTimeItem = existingData.find(item => item.arrivalTime === data.arrivalTime);
+    if (sameTimeItem) {
+      console.warn(`Ya existe una cita a la misma hora y el mismo día: ${data.arrivalTime}.`);
+      return res.status(400).send(`Ya existe una cita a la misma hora y el mismo día: ${data.arrivalTime}.`);
+    }
+  }
+
+  for (const data of dataArray) {
+    const newData = { 
+      ...data, 
+      id: generateUniqueId(data.cedula),
+      status: 'active',
+      image: req.file ? req.file.filename : null 
+    };
+    existingData.push(newData);
+  }
+
+  writeDataToFile(existingData);
+  console.log('Datos guardados correctamente.');
+  res.send('Datos guardados correctamente.');
+});
+
 app.get('/citas', (req, res) => {
-    const { startDate, endDate } = req.query;
-  
-    console.log('startDate:', startDate);
-    console.log('endDate:', endDate);
-  
-    if (!startDate || !endDate) {
-      return res.status(400).send('Debe proporcionar startDate y endDate en la consulta.');
-    }
-  
-    const start = new Date(startDate.split('T')[0]);
-    const end = new Date(endDate.split('T')[0]);
-    end.setUTCDate(end.getUTCDate() + 1); // Incluir todo el día de endDate
-  
-    console.log('start:', start);
-    console.log('end:', end);
-  
-    if (isNaN(start.getTime()) || isNaN(end.getTime())) {
-      console.error('Fechas inválidas:', { start, end });
-      return res.status(400).send('Las fechas proporcionadas no son válidas. Asegúrate de usar el formato ISO 8601.');
-    }
-  
-    const existingData = readDataFromFile();
-  
-    console.log('Datos existentes:', existingData);
-  
-    const filteredCitas = existingData.filter(cita => {
-      const citaDate = new Date(cita.arrivalTime.split('T')[0]);
-      console.log('citaDate:', citaDate);
-      return citaDate >= start && citaDate < end;
-    });
-  
-    console.log('filteredCitas:', filteredCitas);
-  
-    // Añadir URL completa de la imagen a cada cita
-    const citasConImagen = filteredCitas.map(cita => ({
-      ...cita,
-      imageUrl: cita.image ? `http://localhost:3000/uploads/${cita.image}` : null
-    }));
-  
-    res.json(citasConImagen);
+  const { startDate, endDate } = req.query;
+
+  console.log('startDate:', startDate);
+  console.log('endDate:', endDate);
+
+  if (!startDate || !endDate) {
+    return res.status(400).send('Debe proporcionar startDate y endDate en la consulta.');
+  }
+
+  const start = new Date(startDate.split('T')[0]);
+  const end = new Date(endDate.split('T')[0]);
+  end.setUTCDate(end.getUTCDate() + 1);
+
+  console.log('start:', start);
+  console.log('end:', end);
+
+  if (isNaN(start.getTime()) || isNaN(end.getTime())) {
+    console.error('Fechas inválidas:', { start, end });
+    return res.status(400).send('Las fechas proporcionadas no son válidas. Asegúrate de usar el formato ISO 8601.');
+  }
+
+  const existingData = readDataFromFile();
+
+  console.log('Datos existentes:', existingData);
+
+  const filteredCitas = existingData.filter(cita => {
+    const citaDate = new Date(cita.arrivalTime.split('T')[0]);
+    console.log('citaDate:', citaDate);
+    return citaDate >= start && citaDate < end;
   });
-  
+
+  console.log('filteredCitas:', filteredCitas);
+
+  const citasConImagen = filteredCitas.map(cita => ({
+    ...cita,
+    imageUrl: cita.image ? `http://localhost:3000/uploads/${cita.image}` : null
+  }));
+
+  res.json(citasConImagen);
+});
+
+app.delete('/cancel/:id', (req, res) => {
+  const id = req.params.id;
+  let existingData = readDataFromFile();
+  const appointment = existingData.find((appointment) => appointment.id === id);
+
+  if (!appointment) {
+    return res.status(404).send('No se encontró una cita con el ID proporcionado.');
+  }
+
+  appointment.status = 'cancelled';
+  writeDataToFile(existingData);
+  res.json({ message: 'Cita cancelada correctamente.', appointmentId: appointment.id });
+});
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
